30e4f38b43ca0763fd0819097733c9d6eff95801
Augusto César <oprofaugustocesar@gmail.com>
2024-11-18 21:41:56 -0300

    FormAtualizacao

--- clientes/formAtualizacao.php
+++ clientes/formAtualizacao.php
@@ -35,43 +35,43 @@
             <input type="text" name="txtCpf" id="txtCpf" value="<?php echo $cliente->cpf; ?>">
 
             <label for="txtNome">Nome do Cliente</label>
-            <input type="text" name="txtNome" id="txtNome">
+            <input type="text" name="txtNome" id="txtNome" value="<?php echo $cliente->nome; ?>">
 
             <label for="txtEmail">E-mail</label>
-            <input type="email" name="txtEmail" id="txtEmail">
+            <input type="email" name="txtEmail" id="txtEmail" value="<?php echo $cliente->email; ?>">
 
             <label for="txtFone">Telefone</label>
-            <input type="tel" name="txtFone" id="txtFone">
+            <input type="tel" name="txtFone" id="txtFone" value="<?php echo $cliente->fone; ?>">
 
             <label for="txtDataNasc">Data de Nascimento</label>
-            <input type="date" name="txtDataNasc" id="txtDataNasc">
+            <input type="date" name="txtDataNasc" id="txtDataNasc" value="<?php echo $cliente->data_nascimento; ?>">
 
             <label for="txtCidade">Cidade</label>
-            <input type="text" name="txtCidade" id="txtCidade">
+            <input type="text" name="txtCidade" id="txtCidade" value="<?php echo $cliente->cidade; ?>">
 
             <label>Sexo</label>
             
             <div>
-                <input type="radio" name="txtSexo" id="txtSexoM" value="M">
+                <input type="radio" name="txtSexo" id="txtSexoM" value="M" <?php if ($cliente->sexo == 'M') { echo 'checked'; } ?> >
                 <label for="txtSexoM">Masculino</label>
 
-                <input type="radio" name="txtSexo" id="txtSexoF" value="F">
+                <input type="radio" name="txtSexo" id="txtSexoF" value="F" <?php if ($cliente->sexo == 'F') { echo 'checked'; } ?> >
                 <label for="txtSexoF">Feminino</label>
             </div>
 
             <label for="txtRenda">Renda</label>
-            <input type="text" name="txtRenda" id="txtRenda">
+            <input type="text" name="txtRenda" id="txtRenda" value="<?php echo $cliente->renda; ?>">
 
             <label for="txtSenha">Senha</label>
             <input type="password" name="txtSenha" id="txtSenha">
 
             <div style="margin-top: 20px">
-                <input type="checkbox" name="txtBloqueado" id="txtBloqueado">
+                <input type="checkbox" name="txtBloqueado" id="txtBloqueado" <?php if ($cliente->bloqueado == 1) { echo 'checked'; } ?> >
                 <label for="txtBloqueado">Bloqueado</label>
             </div>
             
             <label for="txtObs">Observações</label>
-            <textarea name="txtObs" id="txtObs"></textarea>
+            <textarea name="txtObs" id="txtObs"><?php echo $cliente->obs; ?></textarea>
 
             <button type="submit">Salvar</button>
         </form>
